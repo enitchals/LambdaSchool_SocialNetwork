a51c64626f870f7980e020ed85819c1800dcf480
Yasin Shuman <email@yasin.io>
2018-01-04 14:17:49 -0800

    added forms and tested registration locally via postman. was not able to make call from form i got a creategot an error when i switched url in axios.post to localhost:3030/new-user

--- lsn_frontend/src/components/Registration/Registration.js
+++ lsn_frontend/src/components/Registration/Registration.js
@@ -1,24 +1,160 @@
-import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
-import './Registration.css';
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import { Button, Form, FormGroup, Label, Input, FormText } from "reactstrap";
+import "./Registration.css";
+import axios from "axios";
 
-class Registration extends Component {
-    constructor() {
-        super();
-        this.state = {
+const URL = "localhost:3030";
+class Registration extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      loginEmail: "",
+      loginPw: "",
+      fName: "",
+      lName: "",
+      regEmail: "",
+      regPw: ""
+    };
+  }
 
-        }
-    }
+  onChange = e => {
+    const state = this.state;
+    state[e.target.name] = e.target.value;
+    this.setState(state);
+    console.log(this.state.role);
+    console.log(this.state.loginPw);
+  };
 
-    componentDidMount() {
+  signIn = e => {
+    e.preventDefault();
+    const { loginEmail, loginPw } = this.state;
 
-    }
+    axios
+      .post("URL/login", { email: loginEmail, password: loginPw })
+      .then(result => {});
+  };
 
-    render() {
-        return(
-            <div>Registration Placeholder</div>
-        );
-    }
+  register = e => {
+    e.preventDefault();
+    const { role, fName, lName, regEmail, regPw } = this.state;
+
+    axios
+      .post("URL/new-user", {
+        name: `${fName} ${lName}`,
+        email: regEmail,
+        password: regPw,
+        role: role
+      })
+      .then(result => {});
+  };
+
+  componentDidMount() {}
+
+  render() {
+    return (
+      <div className="login">
+        <div className="login-form">
+          <h1>Login</h1>
+          <Form id="login">
+            <FormGroup>
+              <Label for="loginEmail">Email</Label>
+              <Input
+                type="email"
+                name="loginEmail"
+                id="loginEmail"
+                placeholder="Email"
+                onChange={this.onChange}
+                required
+              />
+            </FormGroup>
+            <FormGroup>
+              <Label for="loginPw">Password</Label>
+              <Input
+                type="password"
+                name="loginPw"
+                id="loginPw"
+                placeholder="password"
+                onChange={this.onChange}
+                required
+              />
+            </FormGroup>
+            <Button onClick={this.signIn} color="primary">
+              Login
+            </Button>
+          </Form>
+        </div>
+        <div className="reg-form">
+          <h1>Register</h1>
+          <Form id="register">
+            <FormGroup>
+              <Label for="role">Role</Label>
+              <Input
+                type="select"
+                name="select"
+                name="role"
+                onChange={this.onChange}
+                required
+              >
+                <option>-</option>
+                <option>student</option>
+                <option>instructor</option>
+                <option>TA</option>
+                <option>graduate</option>
+              </Input>
+            </FormGroup>
+            <FormGroup>
+              <Label for="fName">First Name</Label>
+              <Input
+                type="fName"
+                name="fName"
+                id="fName"
+                placeholder="First Name"
+                onChange={this.onChange}
+                required
+              />
+            </FormGroup>
+            <FormGroup>
+              <Label for="lName">Last Name</Label>
+              <Input
+                type="lName"
+                name="lName"
+                id="lName"
+                placeholder="Last Name"
+                onChange={this.onChange}
+                required
+              />
+            </FormGroup>
+            <FormGroup>
+              <Label for="regEmail">Email</Label>
+              <Input
+                type="email"
+                name="email"
+                id="regEmail"
+                placeholder="Email"
+                onChange={this.onChange}
+                required
+              />
+            </FormGroup>
+            <FormGroup>
+              <Label for="regPw">Password</Label>
+              <Input
+                type="password"
+                name="password"
+                id="regPw"
+                placeholder="password"
+                onChange={this.onChange}
+                required
+              />
+            </FormGroup>
+            <Button onClick={this.register} color="primary">
+              Register
+            </Button>
+          </Form>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default Registration;
