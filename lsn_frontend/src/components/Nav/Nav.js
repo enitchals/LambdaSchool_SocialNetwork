a2e9089052b10063e2e429839b1132e3b5702196
Moises Dobarganes <designerexpert@gmail.com>
2018-01-04 15:28:38 -0500

    Search Functionality prototyped in NavBar

--- lsn_frontend/src/components/Nav/Nav.js
+++ lsn_frontend/src/components/Nav/Nav.js
@@ -6,8 +6,29 @@ class Nav extends Component {
     constructor() {
         super();
         this.state = {
-
+            searchTerm: '',
+            searchWord: '...search'
         }
+    }
+    handleClick = (evt) => {
+        this.setState({searchWord: ''});
+    }
+
+    handleReturn = (evt) => {
+        if (evt.charCode == 13) {
+            console.log('Enter was pressed')
+            this.setState({searchTerm: evt.target.value, searchWord: ''});
+            console.log('search word is now: ', this.state.searchWord);
+        }
+    }
+
+    handleChange = (evt) => {
+        this.setState({searchWord: evt.target.value});
+    }
+
+    handleSubmit = (evt) => {
+        const search = evt.target.value;
+        this.setState({searchWord: search, searchWord: ''});
     }
 
     componentDidMount() {
@@ -20,7 +41,7 @@ class Nav extends Component {
                 <div className='NavWrap'>
                     <div className='LeftNav'>
                         <NavLink className='NavHome' to='/profile'><img src='/img/Lambda_Social.png'/></NavLink>
-                        <input type='text' placeholder='search...' className='NavSearch'/>
+                        <input type='text' value={this.state.searchWord} className='NavSearch' onClick={this.handleClick} onKeyPress={this.handleReturn} onChange={this.handleChange}/>
                         <NavLink className='NavLink' activeClassName='NavLink--selected' to='/profile'>Profile</NavLink>
                         <NavLink className='NavLink' activeClassName='NavLink--selected' to='/list'>Members</NavLink>
                     </div>
