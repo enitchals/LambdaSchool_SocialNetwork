97765811290052898aaf32e507fa606e84058b88
Moises Dobarganes <designerexpert@gmail.com>
2018-01-05 14:51:11 -0500

    Work Updated by Jas

--- lsn_frontend/src/components/Status/Status.js
+++ lsn_frontend/src/components/Status/Status.js
@@ -1,18 +1,47 @@
 import React, { Component } from 'react';
-import './Status.css'
+import './Status.css';
+import {connect} from 'react-redux';
+import {addPostDetails} from '../../actions';
 
 class Status extends Component{
-    constructor(){
-        super()
+    constructor(props){
+        super(props);
+        this.state = {
+            newPost:{
+                text:'',
+                author:this.props.author
+            }
+        }
     }
 
+    handleTextInput=(event) =>{
+        this.setState({newPost:{
+            text:event.target.value,
+            author:this.props.author
+        }});
+    }
+
+    addPostDetails = (event) => {
+        event.preventDefault();
+        this.props.addPostDetails(this.state.newPost);
+        this.setState({
+            newPost:{
+                text:'',
+                author:this.props.author
+            }
+        })
+    }
+   
     render(){
         return (
-            <form onSubmit={this.addComment}>
-                <input className='Postinput' type="text" placeholder="Add new post" onChange={this.handleCommentInput} />
-            </form> 
+        <div>
+                <input className='Postinput' type="text" placeholder="Add new post" onChange={this.handleTextInput} />
+                <button onClick ={this.addPostDetails} > Submit </button>
+            </div>
         )
     }
 }
+
+
 
 export default Status;
